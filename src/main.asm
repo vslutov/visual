28b30dd6884c0f80e675b914e4d47acd05153aec
vslutov <vslutov@yandex.ru>
2014-01-21 01:12:12 +0400

    Add whitespaces

--- src/main.asm
+++ src/main.asm
@@ -31,19 +31,19 @@ segment readable executable
 
 entry $
 
-  mov	edx,msg_size	; CPU zero extends 32-bit operation to 64-bit
+  mov	edx, msg_size ; CPU zero extends 32-bit operation to 64-bit
                     ; we can use less bytes than in case mov rdx,...
-  lea	rsi,[msg]
-  mov	edi,1		; STDOUT
-  mov	eax,1		; sys_write
+  lea	rsi, [msg]
+  mov	edi, 1	  ; STDOUT
+  mov	eax, 1	  ; sys_write
   syscall
 
-  xor	edi,edi 	; exit code 0
-  mov	eax,60		; sys_exit
+  xor	edi, edi  ; exit code 0
+  mov	eax, 60   ; sys_exit
   syscall
 
 segment readable writeable
 
-  msg db 'Hello 64-bit world!',0xA
+  msg db 'Hello 64-bit world!', 0xA
   msg_size = $-msg
 
